78eece19f7567c75642cc0d1f27424a030194780
Jeetesh Meena <66868741+jeeteshmeena@users.noreply.github.com>
2026-03-07 13:53:54 +0530

    Update search_engine.py

--- search_engine.py
+++ search_engine.py
@@ -1,20 +1,6 @@
-from rapidfuzz import process
-from database import load_db
+from database import search_story
+
 
 def fuzzy_search(query):
 
-    db = load_db()
-
-    titles = list(db.keys())
-
-    match = process.extractOne(query, titles)
-
-    if not match:
-        return None
-
-    score = match[1]
-
-    if score < 60:
-        return None
-
-    return db[match[0]]
+    return search_story(query)
