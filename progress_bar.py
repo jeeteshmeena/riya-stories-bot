438ff0646effa785539f89d1d3b89c587f0a45e7
Jeetesh Meena <66868741+jeeteshmeena@users.noreply.github.com>
2026-03-07 13:08:27 +0530

    Update progress_bar.py

--- progress_bar.py
+++ progress_bar.py
@@ -1,12 +1,18 @@
+def progress_bar(step=1):
 
-import asyncio
+    bars = [
+        "░░░░░░░░░░",
+        "██░░░░░░░░",
+        "████░░░░░░",
+        "██████░░░░",
+        "████████░░",
+        "██████████"
+    ]
 
-async def run_progress(update):
-    msg = await update.message.reply_text("🔎 Searching...")
-    await asyncio.sleep(0.3)
-    await msg.edit_text("🔎 Searching...\n██░░░░░░")
-    await asyncio.sleep(0.3)
-    await msg.edit_text("🔎 Searching...\n████░░░░")
-    await asyncio.sleep(0.3)
-    await msg.edit_text("🔎 Searching...\n████████")
-    return msg
+    if step < 0:
+        step = 0
+
+    if step >= len(bars):
+        step = len(bars) - 1
+
+    return bars[step]
