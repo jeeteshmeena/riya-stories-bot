d37a7064c07dd7cbf58abe22cc6aed26c11edeb0
Jeetesh Meena <66868741+jeeteshmeena@users.noreply.github.com>
2026-03-07 13:27:00 +0530

    Update progress_bar.py

--- progress_bar.py
+++ progress_bar.py
@@ -1,18 +1,10 @@
-def progress_bar(step=1):
+def progress_bar(step):
 
     bars = [
-        "░░░░░░░░░░",
-        "██░░░░░░░░",
-        "████░░░░░░",
-        "██████░░░░",
-        "████████░░",
-        "██████████"
+        "🔍 Searching.",
+        "🔍 Searching..",
+        "🔍 Searching...",
+        "🔍 Searching...."
     ]
 
-    if step < 0:
-        step = 0
-
-    if step >= len(bars):
-        step = len(bars) - 1
-
-    return bars[step]
+    return bars[step % len(bars)]
