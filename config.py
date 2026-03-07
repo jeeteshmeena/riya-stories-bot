8857da4a6b2fd312fef3d12f8e957f37e5dd547e
Jeetesh Meena <66868741+jeeteshmeena@users.noreply.github.com>
2026-03-07 12:01:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,10 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 CHANNEL_ID = int(os.getenv("CHANNEL_ID", 0))
 GROUP_ID = int(os.getenv("GROUP_ID", 0))
+
 ADMIN_ID = int(os.getenv("ADMIN_ID", 0))
 
 REQUEST_GROUP = int(os.getenv("REQUEST_GROUP", 0))
+
 COPYRIGHT_CHANNEL = int(os.getenv("COPYRIGHT_CHANNEL", 0))
+LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", 0))
