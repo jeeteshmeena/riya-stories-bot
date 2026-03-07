b3884b8d453f5974448ba375be3ee652b61bb9f0
Jeetesh Meena <66868741+jeeteshmeena@users.noreply.github.com>
2026-03-07 13:09:04 +0530

    Update scanner_client.py

--- scanner_client.py
+++ scanner_client.py
@@ -1,23 +1,8 @@
 from telethon import TelegramClient
 from config import API_ID, API_HASH, SESSION_NAME
 
-client = None
-
-def get_client():
-    global client
-
-    if client is None:
-        client = TelegramClient(SESSION_NAME, API_ID, API_HASH)
-
-    return client
-
-
-async def scan_channel(channel):
-
-    client = get_client()
-
-    await client.start()
-
-    async for msg in client.iter_messages(channel, limit=None):
-
-        print(msg.id)
+client = TelegramClient(
+    SESSION_NAME,
+    API_ID,
+    API_HASH
+)
