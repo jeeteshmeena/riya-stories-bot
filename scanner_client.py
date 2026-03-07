bc149e5c1f652eaa2f08133e0a00b19b384e7fae
Jeetesh Meena <66868741+jeeteshmeena@users.noreply.github.com>
2026-03-07 13:03:40 +0530

    Update scanner_client.py

--- scanner_client.py
+++ scanner_client.py
@@ -1,19 +1,23 @@
 from telethon import TelegramClient
 from config import API_ID, API_HASH, SESSION_NAME
-from parser import parse_story
-from database import add_story
 
-client = TelegramClient(SESSION_NAME, API_ID, API_HASH)
+client = None
+
+def get_client():
+    global client
+
+    if client is None:
+        client = TelegramClient(SESSION_NAME, API_ID, API_HASH)
+
+    return client
+
 
 async def scan_channel(channel):
+
+    client = get_client()
 
     await client.start()
 
     async for msg in client.iter_messages(channel, limit=None):
 
-        story = parse_story(msg)
-
-        if story:
-            add_story(story)
-
-    await client.disconnect()
+        print(msg.id)
