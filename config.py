6176622306f1a9fa9a8c83cf445c1af47e230dcb
Jeetesh Meena <66868741+jeeteshmeena@users.noreply.github.com>
2026-03-07 12:57:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,32 +1,34 @@
 import os
 
-# Bot
+# =========================
+# BOT CONFIG
+# =========================
+
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-# Main Source Channel
 CHANNEL_ID = int(os.getenv("CHANNEL_ID", 0))
 
-# Admin
 ADMIN_ID = int(os.getenv("ADMIN_ID", 0))
 OWNER_ID = int(os.getenv("OWNER_ID", 0))
 
-# Request System
 REQUEST_GROUP = int(os.getenv("REQUEST_GROUP", 0))
 
-# Logs / Copyright
 COPYRIGHT_CHANNEL = int(os.getenv("COPYRIGHT_CHANNEL", 0))
 LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", 0))
 
-# Bot Settings
 BOT_LANGUAGE = os.getenv("BOT_LANGUAGE", "english")
+
 AUTO_SCAN = os.getenv("AUTO_SCAN", "true")
+
 SEARCH_LIMIT = int(os.getenv("SEARCH_LIMIT", 5))
 
 
-# --------------------------------
-# TELEGRAM CLIENT API (Scanner)
-# --------------------------------
+# =========================
+# TELETHON SCANNER CONFIG
+# =========================
 
 API_ID = int(os.getenv("API_ID", 0))
+
 API_HASH = os.getenv("API_HASH")
+
 SESSION_NAME = os.getenv("SESSION_NAME", "riya_session")
