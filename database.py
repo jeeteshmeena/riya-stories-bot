c3463613afa6594fc0c2452592bf6fbff6fe1bd2
Jeetesh Meena <66868741+jeeteshmeena@users.noreply.github.com>
2026-03-09 15:54:24 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,28 +1,46 @@
 import json
+import os
 
 DB_FILE = "stories_db.json"
 
+
 def load_db():
-    try:
-        with open(DB_FILE) as f:
+
+    if os.path.exists(DB_FILE):
+
+        with open(DB_FILE, "r", encoding="utf-8") as f:
             return json.load(f)
-    except:
-        return {}
+
+    return {}
+
 
 def save_db(data):
-    with open(DB_FILE, "w") as f:
-        json.dump(data, f, indent=2)
+
+    with open(DB_FILE, "w", encoding="utf-8") as f:
+        json.dump(data, f, indent=2, ensure_ascii=False)
+
 
 def add_story(story):
+
     db = load_db()
-    db[story["name"].lower()] = story
+
+    name = story["name"]
+
+    if name not in db:
+
+        db[name] = story
+
+    else:
+
+        # latest message logic
+        if story["message_id"] > db[name]["message_id"]:
+            db[name] = story
+
     save_db(db)
 
-def search_story(query):
+
+def get_story(name):
+
     db = load_db()
 
-    for title, data in db.items():
-        if query in title:
-            return data
-
-    return None
+    return db.get(name)
